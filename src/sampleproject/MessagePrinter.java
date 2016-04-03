8914ec64ef2f5fa1f4d44097125725fc23fa576d
Paul <paul@Paul-PC.home>
2016-04-03 08:57:10 +0200

    Add simple MessagePrinter class

---
+++ src/sampleproject/MessagePrinter.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sampleproject;
+
+/**
+ *
+ * @author paul
+ */
+public class MessagePrinter {
+    
+    
+    private String helloFromClass;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String[] options;
+    public MessagePrinter(){
+        System.out.println("Starting MessagePrinter Constructor...");
+      
+        helloFromClass = "Hello from MessagePrinter!";
+        
+        option1 = "Bananas";
+        option2 = "Apples";
+        option3 = "Pears";
+        option4 = "Strawberries";
+        
+        options = new String[4];
+        
+        options[0] = option1;
+        options[1] = option2;
+        options[2] = option3;
+        options[3] = option4;
+        
+        System.out.println("Finished MessagePrinter Constructor...");  
+    }
+    
+    
+    /**
+     * Public messages can be accessed from other classes
+     */
+    public void printMessage(){
+        System.out.println(helloFromClass);
+    }
+    
+    public void printMenuOptions(){
+        System.out.println("pick an option: ");
+        
+        for(int i = 0; i < options.length; i++){
+            System.out.println((i + 1) + ". " + options[i]);
+        }
+        
+        System.out.println("Selection:");
+    }
+    
+    public void printSelection(String selected){
+
+        
+        switch(selected){
+        
+            case "1":
+                System.out.println("You Selected: " + options[0]);
+                break;
+            case "2":
+                System.out.println("You Selected: " + options[1]);
+                break;
+            case "3":
+                System.out.println("You Selected: " + options[2]);
+                break;
+            case "4":
+                System.out.println("You Selected: " + options[3]);
+                break;
+            default:
+                System.out.println("I'm Sorry " + selected + " is not an available option");
+                
+        }
+    }
+}
--- src/sampleproject/Sampleproject.java
+++ src/sampleproject/Sampleproject.java
@@ -5,17 +5,52 @@
  */
 package sampleproject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 /**
  *
  * @author Kenneth
  */
 public class Sampleproject {
 
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("WOOHOO!!");
+       
+       /*
+           Methods in the main class must be static.
+           Meaning they can be called without instantiating the class.
+           See the method signiture below.
+       */
+       printHello();
+       
+       /*
+       There are print messages in the constructor so you can see when it's called
+       */
+       MessagePrinter printer = new MessagePrinter();
+       /*
+        Printing a message from another class
+       */
+       printer.printMessage();
+       printer.printMenuOptions();
+       
+       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String in = reader.readLine();
+            printer.printSelection(in);
+        } catch (IOException ex) {
+         System.out.println("An IOException occurred! ");
+        }
     }
     
+    
+    private static void printHello(){
+        System.out.println("Hello from static method!");
+    }
 }
