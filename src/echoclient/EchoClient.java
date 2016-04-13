587813c6689e53d32a41835671e6246817fdd55d
Kenneth Power <kpower77@gmail.com>
2016-04-13 19:58:38 +0100

    Updated EchoClient

--- src/echoclient/EchoClient.java
+++ src/echoclient/EchoClient.java
@@ -5,10 +5,32 @@
  */
 package echoclient;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 /**
- *
- * @author Kenneth
+ * @date April 2016
+ * @author Kenneth Power
  */
 public class EchoClient {
+    private Socket clientSocket;
+    
+    public void runClient(){
+        try{
+            clientSocket = new Socket("localhost", 40000);
+            System.out.println("Connect to Server on port 40000");
+            
+            ObjectOutputStream out;
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+            
+        }
+        catch(IOException ex){
+            System.out.println("An IOExcepion occured");
+            ex.printStackTrace();
+        }
+    }
     
 }
